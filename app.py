b6c60019846074458db5c7c56a3d372b8541dc55
infantvivek <infantvivek@gmail.com>
2026-03-18 10:06:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,10 +14,11 @@ DSAT_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSU-KDmKs9i1EIEuIuJT
 LOGO_URL = "https://s3.amazonaws.com/cdn.freshdesk.com/data/helpdesk/attachments/production/48175265495/original/PTXBCP40UHx-8LCKsM1zqLX-pq8nndFHSw.png?1641235482"
 
 # --- GOOGLE FORM CONFIGURATION ---
-FORM_ID = "YOUR_GOOGLE_FORM_ID_HERE" # Replace with your actual Form ID
-ENTRY_KEY = "entry.1"       
-ENTRY_TYPE = "entry.2"      
-ENTRY_FEEDBACK = "entry.3"  
+FORM_ID = "1I3pOczioxWQQAbhz3yuqwGNADl9BbJiYnCBJpxNvE7w" # Replace with your actual Form ID
+#https://docs.google.com/forms/d/e/1FAIpQLSdu2gEmHPZBCoUZ1naQlGTeJtgTgB47YfCENCfeKAHU1OA76g/viewform?usp=pp_url&entry.1726897360=test&entry.1303252108=Controllable&entry.1754509958=testbmbnm
+ENTRY_KEY = "entry.1726897360"       
+ENTRY_TYPE = "entry.1303252108"      
+ENTRY_FEEDBACK = "entry.1754509958"  
 
 st.set_page_config(layout="wide", page_title="HighLevel Performance Hub", page_icon="🚀")
 
